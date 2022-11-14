303d74d0718142d8b82b1af635b7edb27c3e475d
n01n9 <sdh20060311@gmail.com>
2022-11-15 02:35:14 +0900

    textbutton and sceeenswitching

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-
+from src.scene.login_scene import LoginScene
 from src.scene.scene import SceneManager
 from src.scene.scene_id import SceneId
 from src.scene.start_scene import StartScene
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 scene_manager = SceneManager(SceneId.start_scene)
 scene_manager.add_scene(SceneId.start_scene, StartScene())
+scene_manager.add_scene(SceneId.login_scene, LoginScene())
 
 running = True
 while running:
--- src/scene/login_scene.py
+++ src/scene/login_scene.py
@@ -1,39 +1,32 @@
 from typing import Sequence, Tuple
 
 import pygame as pg
-
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 from src.entity.abstract_entity import Transform
 from src.entity.button import Button
 from src.scene.scene import Scene
 from src.scene.scene_id import SceneId
 
-
-class StartScene(Scene):
-
-    def nextscene(self):
-        self.switch = SceneId.login_scene
-
-    def __init__(self):
-        self.start_scene()
-
+class LoginScene(Scene):
+        
     def update(self, key_pressed: Sequence[bool], mouse_pos: Tuple[int, int], mouse_click:Tuple[int,int,int]) -> None:
         self.key_pressed = key_pressed
         self.mouse_pos = mouse_pos
         self.mouse_click = mouse_click
-        self.switch = None
-        self.start_button.update(key_pressed,mouse_pos,mouse_click)
+        self.confirm_button.update(key_pressed,mouse_pos,mouse_click)
+        
+        return
     
     def draw(self, screen: pg.surface.Surface) -> None:
         screen.fill((255,255,255))
-        screen.blit(self.start_button.image,self.start_button.rect.topleft)
+        screen.blit(self.confirm_button.image,self.confirm_button.rect.topleft)
 
     def check_scene_switch(self) -> SceneId | None:
-        return self.switch
+        return None
 
     def start_scene(self) -> None:
-        pg.init()
-        self.start_button = Button(pg.image.load('.\image\start_button.png'), 0,0,Transform(0,pg.Vector2(SCREEN_WIDTH/2,SCREEN_HEIGHT/2),pg.Vector2(1,0)), lambda : self.nextscene())
+        self.confirm_button = Button(pg.font.Font(None,35).render('confirm',True,(0,0,0),(255,0,0)), 0,0,Transform(0,pg.Vector2(SCREEN_WIDTH/2,SCREEN_HEIGHT/2),pg.Vector2(1,0)),lambda : print('a'))
 
     def stop_scene(self) -> None:
         return
