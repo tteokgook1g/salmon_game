b3eb443412cff694b5cef951693f8978fe31e872
HK Lee <tteokgook1n@naver.com>
2022-11-16 19:14:19 +0900

    refactoring: dataclass UpdateInfo and edit update method

--- src/entity/textbox.py
+++ src/entity/textbox.py
@@ -1,26 +1,27 @@
-from typing import Sequence, Tuple
+from typing import Tuple
 
 import pygame as pg
-from pygame.event import Event
 from pygame.surface import Surface
 
 from src.entity.abstract_entity import Entity, Transform
+from src.helper.update_info import UpdateInfo
 
 pg.init()
 
 
 class TextBox(Entity):
 
-    def __init__(self, img: Surface, health: int, power: int, transform: Transform, text: str, size:int, color: Tuple[int,int,int]):
+    def __init__(self, img: Surface, health: int, power: int, transform: Transform, text: str, size: int, color: Tuple[int, int, int]):
         super().__init__(img, health, power, transform)
-        self.color= color
+        self.color = color
         self.text = text
         self.font = pg.font.Font(None, size)
-        self.txt_surface = self.font.render(text.center(30,' '), True, self.color)
+        self.txt_surface = self.font.render(
+            text.center(30, ' '), True, self.color)
         self.active = False
         self.coloractive = self.color
 
-    def update(self, key_pressed: Sequence[bool], mouse_pos: Tuple[int, int], mouse_click: Tuple[int, int, int], events: Sequence[Event]) -> None:
+    def update(self, info: UpdateInfo) -> None:
         # Resize the box if the text is too long.
         width = max(200, self.txt_surface.get_width()+10)
         self.rect.w = width
@@ -30,4 +31,5 @@ class TextBox(Entity):
         # Blit the text.
         screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
         # Blit the rect.
-        pg.draw.rect(screen, self.color, pg.rect.Rect(self.rect.x-5,self.rect.y-5,self.rect.w+10,self.rect.h+10),2)
+        pg.draw.rect(screen, self.color, pg.rect.Rect(
+            self.rect.x-5, self.rect.y-5, self.rect.w+10, self.rect.h+10), 2)
--- src/scene/stage_scene.py
+++ src/scene/stage_scene.py
@@ -1,17 +1,16 @@
-from typing import Sequence, Tuple
 import pygame as pg
 from pygame import Vector2
 from pygame.surface import Surface
+
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, WORLD_BORDER
-from src.scene.scene import Scene
+from src.entity.abstract_entity import Transform
+from src.entity.enemy.basicEnemy import Enemy
+from src.entity.player import Player, SkillParticle
 from src.helper.camera_surface import CameraSurface
 from src.helper.group import Group
-from src.entity.player import Player, SkillParticle
-from src.entity.enemy.basicEnemy import Enemy
-
-from src.entity.abstract_entity import Transform
+from src.helper.update_info import UpdateInfo
+from src.scene.scene import Scene
 from src.scene.scene_id import SceneId
-from pygame.event import Event
 
 
 class Stage(Scene):
@@ -30,17 +29,17 @@ class Stage(Scene):
         self.camera_surface = CameraSurface(
             (SCREEN_WIDTH, SCREEN_HEIGHT), player.transform)
         self.switch = None
-        self.background = Surface((WORLD_BORDER,WORLD_BORDER))
-        self.background.fill((240,240,240))
-        
+        self.background = Surface((WORLD_BORDER, WORLD_BORDER))
+        self.background.fill((240, 240, 240))
 
-    def update(self, key_pressed: Sequence[bool], mouse_pos: Tuple[int, int], mouse_click: Tuple[int, int, int], events: Sequence[Event]) -> None:
-        self.player.update(key_pressed, mouse_pos, mouse_click, events)
+    def update(self, info: UpdateInfo) -> None:
+        info.player = self.player
+        self.player.update(info)
         for enemy in self.enemies:
-            enemy.update(key_pressed, mouse_pos, mouse_click, events, self.player)
+            enemy.update(info)
         for particle in self.skill_particles:
-            particle.update(key_pressed, mouse_pos, mouse_click, events)
-        if self.player.health<0:
+            particle.update(info)
+        if self.player.health < 0:
             self.switch = SceneId.end_scene
         self.collide()
 
@@ -52,26 +51,27 @@ class Stage(Scene):
     def collide(self):
         for enemy in self.enemies:
             if pg.sprite.collide_rect(self.player, enemy):
-                self.player.iscollide(enemy)
-                enemy.transform.velocity*=-10
+                self.player.handle_collide(enemy)
+                enemy.transform.velocity *= -10
                 enemy.transform.move()
-                enemy.transform.velocity/=-10
+                enemy.transform.velocity /= -10
         for particle in self.skill_particles:
-            if pg.sprite.collide_rect(self.player,particle):
+            if pg.sprite.collide_rect(self.player, particle):
                 pass
 
     def draw_on_camera_surface(self, camera_surface: CameraSurface) -> None:
         """implement it to draw entities"""
-        border = Surface((WORLD_BORDER+2,WORLD_BORDER+2))
-        border.fill((0,0,0))
-        camera_surface.blit(border,pg.rect.Rect(-1,-1,WORLD_BORDER+2,WORLD_BORDER+2))
-        camera_surface.blit(self.background,pg.rect.Rect(0,0,WORLD_BORDER,WORLD_BORDER))
+        border = Surface((WORLD_BORDER+2, WORLD_BORDER+2))
+        border.fill((0, 0, 0))
+        camera_surface.blit(border, pg.rect.Rect(-1, -1,
+                            WORLD_BORDER+2, WORLD_BORDER+2))
+        camera_surface.blit(self.background, pg.rect.Rect(
+            0, 0, WORLD_BORDER, WORLD_BORDER))
         camera_surface.blit(self.player.image, self.player.rect)
         for enemy in self.enemies:
             camera_surface.blit(enemy.image, enemy.rect)
         for particle in self.skill_particles:
             camera_surface.blit(particle.image, particle.rect)
-        
 
     def check_scene_switch(self) -> SceneId | None:
         return self.switch
@@ -85,5 +85,3 @@ class Stage(Scene):
 
     def stop_scene(self) -> None:
         return
-
-
