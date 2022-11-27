823a312c104d99032a0ddbeeaa0b1103f8d79cd4
HK Lee <tteokgook1n@naver.com>
2022-11-27 23:55:14 +0900

    lightning bugfix

--- src/entity/player.py
+++ src/entity/player.py
@@ -95,8 +95,6 @@ class BombParticle(SkillParticle):
     def update(self, info: UpdateInfo) -> None:
         super().update(info)
         if self.active:
-            # self.view_radius += self.max_radius * \
-            # (15/16)/(self.explosion_time*60)
             self.view_radius = lerp(self.view_radius, self.max_radius, 0.12)
             self.image = pg.transform.scale(
                 self.explosion_img, (self.view_radius*2, self.view_radius*2))
@@ -124,7 +122,7 @@ class LightningParticle(SkillParticle):
     lifetime = 0.5
 
     def __init__(self, img: Surface, power: float, player: Player) -> None:
-        transform = Transform(
+        self.transform = Transform(
             0, pg.Vector2(player.transform.pos.xy), pg.Vector2(
                 pg.mouse.get_pos())-pg.Vector2(SCREEN_WIDTH, SCREEN_HEIGHT)/2
         )
@@ -136,7 +134,7 @@ class LightningParticle(SkillParticle):
         img_rect.center = self.rect.center
         lightning_img.blit(self.light_img, img_rect)
 
-        super().__init__(lightning_img, power, transform)
+        super().__init__(lightning_img, power, self.transform)
         schedule.every(self.lifetime).seconds.do(self.kill)  # type: ignore
 
     def get_arc(self):
